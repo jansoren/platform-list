bd1c7f4921bf17de6fed9ddc688435cd1d3b3eb6
Geninsus <courmontguillaume@gmail.com>
2026-02-17 10:58:54 +0700

    add turbine cash platform

---
+++ src/platforms/turbinecash.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "turbinecash",
+  name: "Turbine Cash",
+  description:
+    "Turbine Cash is a privacy-focused DeFi platform on Solana using zero-knowledge cryptography to obfuscate transactions, offering private swaps and DCA.",
+  tags: ["dapp", "defi", "privacy"],
+  links: {
+    website: "https://turbine.cash/",
+    twitter: "https://x.com/turbine_cash",
+  },
+};
+
+export const services: ServiceRaw[] = [];
